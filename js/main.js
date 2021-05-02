6bfbed6233089a1a6b136e94ccdaebaf1fc95ae9
habib <habibdev96@gmail.com>
2021-05-02 17:38:54 -0600

    refactored javascript code

--- js/main.js
+++ js/main.js
@@ -1,22 +1,38 @@
 const btns = [...document.querySelectorAll(".btn")];
 const cardImg = [...document.querySelectorAll(".card-img")];
 
+function mouseoverFocusAnimation(e) {
+  cardImg.forEach((img) => {
+    if (e.target.dataset.id === img.dataset.id) {
+      img.style.transform = "translate(100%, 100%) scale(1.5)";
+      img.style.opacity = 0.5;
+    }
+  });
+}
+
+function mouseoutFocusOutAnimation(e) {
+  cardImg.forEach((img) => {
+    if (e.target.dataset.id === img.dataset.id) {
+      img.style.transform = "translateX(0) scale(1)";
+      img.style.opacity = 1;
+    }
+  });
+}
+
 btns.forEach((btn) => {
   btn.addEventListener("mouseover", (e) => {
-    cardImg.forEach((img) => {
-      if (e.target.dataset.id === img.dataset.id) {
-        img.style.transform = "translate(100%, 100%) scale(1.5)";
-        img.style.opacity = 0.5;
-      }
-    });
+    mouseoverFocusAnimation(e);
+  });
+
+  btn.addEventListener("focus", (e) => {
+    mouseoverFocusAnimation(e);
   });
 
   btn.addEventListener("mouseout", (e) => {
-    cardImg.forEach((img) => {
-      if (e.target.dataset.id === img.dataset.id) {
-        img.style.transform = "translateX(0) scale(1)";
-        img.style.opacity = 1;
-      }
-    });
+    mouseoutFocusOutAnimation(e);
+  });
+
+  btn.addEventListener("focusout", (e) => {
+    mouseoutFocusOutAnimation(e);
   });
 });
